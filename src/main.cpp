b9b3e068e80cc1a4e1e78762743844d624ebbf3e
Nils Grothmann <nils.grothmann@gmail.com>
2025-08-16 12:50:06 +0200

    1.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <Joystick.h>
+#include "HX711.h"
 
 // Joystick initialisieren:
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,
@@ -13,15 +14,20 @@ Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,
                    true,                // Brake
                    false);              // Steering
 
-const int pinBrake = A0;
+const int pinClutch = A0;
 const int pinThrottle = A1;
+const int DT_PIN = 5;
+const int SCK_PIN = 4;
 
 // 🔧 Kalibrierung
-const int brakeMin = 579;
-const int brakeMax = 693;
+const int clutchMin = 579;
+const int clutchMax = 693;
 
 const int throttleMin = 619;
-const int throttleMax = 804;
+const int throttleMax = 770;
+
+const long brakeMin = 46400;
+const long brakeMax = 180000;
 
 // ⚙️ Deadzone in beiden Richtungen um 0
 const int deadzone = 20;
@@ -34,32 +40,42 @@ int applyDeadzone(int value, int center, int width) {
   return value;
 }
 
+HX711 scale;
+
 void setup() {
   Serial.begin(115200);
+  scale.begin(DT_PIN, SCK_PIN);
   Joystick.begin();
 }
 
 void loop() {
   // Rohdaten einlesen
-  int brakeRaw = analogRead(pinBrake);
+  int clutchRaw = analogRead(pinClutch);
   int throttleRaw = analogRead(pinThrottle);
+  long brakeRaw = scale.read();
 
   // Kalibrierung auf 0–1023
-  int brake = map(constrain(brakeRaw, brakeMin, brakeMax), brakeMin, brakeMax, 0, 1023);
+  int clutch = map(constrain(clutchRaw, clutchMin, clutchMax), clutchMin, clutchMax, 0, 1023);
   int throttle = map(constrain(throttleRaw, throttleMin, throttleMax), throttleMin, throttleMax, 0, 1023);
+  int brake = map(constrain(brakeRaw, brakeMin, brakeMax), brakeMin, brakeMax, 0, 1023);
 
   // Deadzone um 0 anwenden
-  brake = applyDeadzone(brake, 0, deadzone);
+  clutch = applyDeadzone(clutch, 0, deadzone);
   throttle = applyDeadzone(throttle, 0, deadzone);
+  brake = applyDeadzone(brake, 0, deadzone);
 
   // An Joystick senden
-  Joystick.setBrake(brake);
+  Joystick.setAccelerator(clutch);
   Joystick.setThrottle(throttle);
+  Joystick.setBrake(brake);
 
   // Serielle Ausgabe für Plotter
-  Serial.print(brake);
+  Serial.print(clutch);
   Serial.print('\t');
-  Serial.println(throttle);
+  Serial.print(throttle);
+  Serial.print('\t');
+  Serial.println(brake);
+
 
   delay(10);
 }
